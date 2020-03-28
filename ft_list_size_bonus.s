e697f731573e8db98cac529366db4eca2ed6ccbd
lulebugl <lulebugl@student.42.fr>
2020-03-28 10:16:17 +0100

    Working mac version

---
+++ ft_list_size_bonus.s
@@ -0,0 +1,8 @@
+; ft_list_size
+
+	segment .text
+	global	ft_list_size
+
+ft_list_size:
+	xor	rax, rax
+	
--- ft_strdup.s
+++ ft_strdup.s
@@ -8,19 +8,19 @@
 
 _ft_strdup:
 	cmp 	rdi, 0
-	je	error
-	call 	_ft_strlen
-	inc 	rax
-	push	rdi
-	mov		rdi, rax
-	call	_malloc
+	je		error		; check if 1st argument exist
+	call 	_ft_strlen	; get string len in rax
+	inc 	rax			; increment rax for last 0
+	push	rdi			; mov 1st arg to the stack
+	mov		rdi, rax	
+	call	_malloc		; malloc size of rdi 
 	cmp		rax, 0
 	je		error
-	pop		rdi
-	push	rsi
-	mov 	rsi, rdi
-	mov		rdi, rax
-	call	_ft_strcpy
+	pop		rdi			; rdi back from the stack
+	push	rsi			
+	mov 	rsi, rdi	; mov the string we want to copy in rsi(src fo strcpy)
+	mov		rdi, rax	; mov the string malloc(ed) in rdi before the copy
+	call	_ft_strcpy	; return the string copied (in rax)
 	pop		rsi
 	ret
 	
